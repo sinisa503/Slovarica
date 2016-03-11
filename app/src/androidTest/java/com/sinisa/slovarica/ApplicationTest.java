25235d7e3198d200ed72eace6ae437034d5cd19e
Sinisa Vukovic <sinisa503@gmail.com>
2016-03-11 13:16:03 +0100

    Initial_Commit

---
+++ app/src/androidTest/java/com/sinisa/slovarica/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.sinisa.slovarica;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a_png href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a_png>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/com/sinisa/slovarica/UcimActivity.java
@@ -0,0 +1,83 @@
+package com.sinisa.slovarica;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+
+/**
+ * Created by SINISA on 8.3.2016..
+ */
+public class UcimActivity extends AppCompatActivity {
+
+    ImageButton forward, backward;
+    FrameLayout frameLayout;
+    ResIds ids = new ResIds();
+    int number = 0;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ucim_activity);
+        setUpToolbar();
+
+        forward = (ImageButton)findViewById(R.id.forward);
+        backward = (ImageButton)findViewById(R.id.backward);
+        frameLayout = (FrameLayout)findViewById(R.id.frameLayout);
+        frameLayout.setBackgroundResource((Integer) ids.getIds().get(number));
+
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number++;
+                if (number<0){
+                    number = 29;
+                }
+                if (number > 29){
+                    number = 0;
+                }
+                frameLayout.setBackgroundResource((Integer) ids.getIds().get(number));
+            }
+        });
+
+        forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number++;
+                if (number<0){
+                    number = 29;
+                }
+                if (number > 29){
+                    number = 0;
+                }
+                frameLayout.setBackgroundResource((Integer) ids.getIds().get(number));
+            }
+        });
+
+        backward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                number--;
+                if (number<0){
+                    number = 29;
+                }
+                if (number > 29){
+                    number = 0;
+                }
+                frameLayout.setBackgroundResource((Integer) ids.getIds().get(number));
+            }
+        });
+
+    }
+
+    private void setUpToolbar() {
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+}
